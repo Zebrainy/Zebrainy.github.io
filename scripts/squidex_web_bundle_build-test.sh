9dcac69b1ad2c8db82af5f355da0324de39ac262
Evgeniy L <39852526+Jnqa@users.noreply.github.com>
2022-05-27 18:33:05 +0300

    Revert "Spc-11210 zip size squidex"

--- scripts/squidex_web_bundle_build-test.sh
+++ scripts/squidex_web_bundle_build-test.sh
@@ -6,6 +6,7 @@ echo "current time: $TIME_NOW"
 echo "build url: $4"
 echo "client-version id: $5"
 echo "bundle type: $6"
+echo "zipSize: $7"
 
 if [ -z "$1" ]; then
         echo "Squidex token not provided"
@@ -33,8 +34,9 @@ curl -XPOST -H 'Authorization: Bearer '$1'' -H "Content-type: application/json" 
   "name":{"iv":"'$2'"},
   "version":{"iv":"'$3'"},
   "zip":{"iv":"'$4'"},
+  "zipSize":{"iv":"'$7'"},
   "buildDate":{"iv":"'$TIME_NOW'"}
-}' 'https://cloud.squidex.io/api/content/skazbuka/web-bundle-build/?publish=true' > squidex-output.json
+}' 'https://cloud.squidex.io/api/content/skazbuka/web-bundle-build-test/?publish=true' > squidex-output.json
 
 BUILD_ID="$(cat squidex-output.json | jq --raw-output '.id')"
 
@@ -47,8 +49,6 @@ if [ -z "$6" ]; then
         exit 0
 fi
 
-echo "build id: $BUILD_ID"
-
-curl -XPOST -H 'Authorization: Bearer '$1'' -H "Content-type: application/json" -d '{
-  "'$6'":{"iv":["'$BUILD_ID'"]}
-}' 'https://prod.zebr-a.com/squidex/tools/draft/client-versions/'$5'/patch'
+# curl -XPOST -H 'Authorization: Bearer '$1'' -H "Content-type: application/json" -d '{
+#   "'$6'":{"iv":["'$BUILD_ID'"]}
+# }' 'https://prod.zebr-a.com/squidex/tools/draft/client-versions/'$5'/patch'
