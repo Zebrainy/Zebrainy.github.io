7c4e03eeec36c5060161cdb8514ae03be27313b3
Evgeniy L <39852526+Jnqa@users.noreply.github.com>
2022-06-16 22:50:10 +0300

    smaller z
    fix SPC-11210

--- scripts/squidex_web_bundle_build.sh
+++ scripts/squidex_web_bundle_build.sh
@@ -35,7 +35,7 @@ curl -XPOST -H 'Authorization: Bearer '$1'' -H "Content-type: application/json" 
   "version":{"iv":"'$3'"},
   "zip":{"iv":"'$4'"},
   "buildDate":{"iv":"'$TIME_NOW'"},
-  "ZipSize":{"iv":"'$7'"}
+  "zipSize":{"iv":"'$7'"}
 }' 'https://cloud.squidex.io/api/content/skazbuka/web-bundle-build/?publish=true' > squidex-output.json
 
 BUILD_ID="$(cat squidex-output.json | jq --raw-output '.id')"
