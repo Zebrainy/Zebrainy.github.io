f00098827f689689890226d0f9608a489e5a1b6c
dmi <dmi.villione@gmail.com>
2023-05-22 19:22:28 +0300

    upd script

--- scripts/map_tag_to_k8s_branch.js
+++ scripts/map_tag_to_k8s_branch.js
@@ -3,25 +3,21 @@ let [, , GITHUB_TAG, ENV] = process.argv
 if (!GITHUB_TAG) console.error(`please provide github tag!`), process.exit(1)
 if (!ENV) console.error(`please provide env!`), process.exit(1)
 
-const dev = {
-    "dev-yc-v": "dev-yc",
-    "yc-v": "dev-yc",
-    "do-v": "dev",
-    "dev-do-v": "dev",
-}
-const prod = {
-    "dev-yc-v": "",
-    "yc-v": "prod-yc",
-    "do-v": "master",
-    "dev-do-v": "",
-}
-if (ENV === "dev") {
-    console.log(dev[GITHUB_TAG])
-    process.exit(0)
-}
-if (ENV === "prod") {
-    console.log(prod[GITHUB_TAG])
-    process.exit(0)
+const tags = {
+	dev: {
+		"dev-yc-v": "dev-yc",
+		"yc-v": "dev-yc",
+		"do-v": "dev",
+		"dev-do-v": "dev",
+	},
+	prod: {
+		"dev-yc-v": "",
+		"yc-v": "prod-yc",
+		"do-v": "master",
+		"dev-do-v": "",
+	},
 }
 
-console.log(branches[GITHUB_TAG])
+const find = Object.keys(tags[ENV]).find((key) => key.startsWith(GITHUB_TAG))
+console.log(console.log(find))
+process.exit(0)
