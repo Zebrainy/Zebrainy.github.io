cbec185268e72d0ccfa0d45b3df4b0e5520d9547
dmi <dmi.villione@gmail.com>
2023-05-22 21:06:49 +0300

    add relace

--- scripts/map_tag_to_k8s_branch.js
+++ scripts/map_tag_to_k8s_branch.js
@@ -3,6 +3,8 @@ let [, , GITHUB_TAG, ENV] = process.argv
 if (!GITHUB_TAG) console.error(`please provide github tag!`), process.exit(1)
 if (!ENV) console.error(`please provide env!`), process.exit(1)
 
+GITHUB_TAG = GITHUB_TAG.replace(/refs\/.*\//, "")
+
 const tags = {
 	dev: {
 		"dev-yc-v": "dev-yc",
@@ -18,6 +20,7 @@ const tags = {
 	},
 }
 
-const find = Object.keys(tags[ENV]).find((key) => GITHUB_TAG.startsWith(key))
-console.log(find)
+const obj = tags[ENV]
+const find = Object.keys(obj).find((key) => GITHUB_TAG.startsWith(key))
+console.log(obj[find])
 process.exit(0)
