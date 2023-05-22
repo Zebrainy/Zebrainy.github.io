9329e87e24cce9c1f80d7ec41d1fb57924ec4b07
dmi <dmi.villione@gmail.com>
2023-05-22 17:07:37 +0300

    upd script

--- scripts/parse_github_ref.js
+++ scripts/parse_github_ref.js
@@ -2,5 +2,12 @@ let [, , GITHUB_REF] = process.argv
 
 if (!GITHUB_REF) console.error(`please provide github ref!`), process.exit(1)
 
+const tags = {
+	"dev-yc": "dev-yc-v",
+	"main-yc": "yc-v",
+	main: "v",
+	dev: "dev-v",
+}
+
 let [, , ...rest] = GITHUB_REF.split("/")
-console.log(rest.join("/"))
+console.log(tags[rest.join("/")])
