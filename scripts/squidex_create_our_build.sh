dec81239ae269d2d1565f7091e1d6875f10f395f
Evgeniy L <148352995+jnqa3@users.noreply.github.com>
2023-11-13 12:00:09 +0300

    update scripts with our squidex
    new squidex

---
+++ scripts/squidex_create_our_build.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+TIME_NOW="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+echo "game: $2"
+echo "version: $3"
+echo "current time: $TIME_NOW"
+echo "build url: $4"
+echo "catalog-game id: $5"
+echo "zipSize: $6"
+echo "protocol-url: $7"
+
+if [ -z "$1" ]; then
+        echo "Squidex token not provided"
+        exit 1
+fi
+
+if [ -z "$2" ]; then
+        echo "Game name not provided"
+        exit 1
+fi
+
+if [ -z "$3" ]; then
+        echo "Game version not provided"
+        exit 1
+fi
+
+if [ -z "$4" ]; then
+        echo "Build URL not provided"
+        exit 1
+fi
+
+set -e
+
+curl -XPOST -H 'Authorization: Bearer '$1'' -H "Content-type: application/json" -d '{
+  "gameId":{"iv":"'$2'"},
+  "version":{"iv":"'$3'"},
+  "url":{"iv":"'$4'"},
+  "buildDate":{"iv":"'$TIME_NOW'"},
+  "zipSize":{"iv":"'$6'"},
+  "protocol-url":{"iv":"'$7'"}
+}' 'https://squidex.zebr-a.com/api/content/skazbuka/web-game-build/?publish=true' > squidex-output.json
+
+BUILD_ID="$(cat squidex-output.json | jq --raw-output '.id')"
+echo "BUILD_ID: $BUILD_ID"
+Response="$(cat squidex-output.json)"
+echo "Response: $Response"
+
+if [ $BUILD_ID == "null" ]; then
+        echo "Build_ID error. Curl without zipSize"
+        curl -XPOST -H 'Authorization: Bearer '$1'' -H "Content-type: application/json" -d '{
+        "gameId":{"iv":"'$2'"},
+        "version":{"iv":"'$3'"},
+        "url":{"iv":"'$4'"},
+        "buildDate":{"iv":"'$TIME_NOW'"}
+        }' 'https://squidex.zebr-a.com/api/content/skazbuka/web-game-build/?publish=true' > squidex-output.json
+        BUILD_ID="$(cat squidex-output.json | jq --raw-output '.id')"
+        echo "BUILD_ID: $BUILD_ID"
+        Response="$(cat squidex-output.json)"
+        echo "Response: $Response"               
+fi
+
+
+if [ -z "$5" ]; then
+        echo "catalog-game id not provided, exiting"
+        exit 0
+fi
+
+echo "build id: $BUILD_ID"
+
+curl -XPOST -H 'Authorization: Bearer '$1'' -H "Content-type: application/json" -d '{
+  "build":{"iv":["'$BUILD_ID'"]}
+}' 'https://prod.zebr-a.com/squidex/tools/draft/catalog-game/'$5'/patch'
---
+++ scripts/squidex_web_bundle_our_build.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+TIME_NOW="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+echo "web-bundle: $2"
+echo "version: $3"
+echo "current time: $TIME_NOW"
+echo "build url: $4"
+echo "client-version id: $5"
+echo "bundle type: $6"
+echo "zipSize: $7"
+
+if [ -z "$1" ]; then
+        echo "Squidex token not provided"
+        exit 1
+fi
+
+if [ -z "$2" ]; then
+        echo "Bundle name not provided"
+        exit 1
+fi
+
+if [ -z "$3" ]; then
+        echo "Bundle version not provided"
+        exit 1
+fi
+
+if [ -z "$4" ]; then
+        echo "Build URL not provided"
+        exit 1
+fi
+
+set -e
+
+curl -XPOST -H 'Authorization: Bearer '$1'' -H "Content-type: application/json" -d '{
+  "name":{"iv":"'$2'"},
+  "version":{"iv":"'$3'"},
+  "zip":{"iv":"'$4'"},
+  "buildDate":{"iv":"'$TIME_NOW'"},
+  "zipSize":{"iv":"'$7'"}
+}' 'https://squidex.zebr-a.com/api/content/skazbuka/web-bundle-build/?publish=true' > squidex-output.json
+
+BUILD_ID="$(cat squidex-output.json | jq --raw-output '.id')"
+echo "BUILD_ID: $BUILD_ID"
+Response="$(cat squidex-output.json)"
+echo "Response: $Response"
+
+if [ $BUILD_ID == "null" ]; then
+        echo "Build_ID error. Curl without zipSize"
+        curl -XPOST -H 'Authorization: Bearer '$1'' -H "Content-type: application/json" -d '{
+        "name":{"iv":"'$2'"},
+        "version":{"iv":"'$3'"},
+        "zip":{"iv":"'$4'"},
+        "buildDate":{"iv":"'$TIME_NOW'"}
+        }' 'https://squidex.zebr-a.com/api/content/skazbuka/web-bundle-build/?publish=true' > squidex-output.json 
+        BUILD_ID="$(cat squidex-output.json | jq --raw-output '.id')"
+        echo "BUILD_ID: $BUILD_ID"
+        Response="$(cat squidex-output.json)"
+        echo "Response: $Response"               
+fi
+
+
+if [ -z "$5" ]; then
+        echo "client-version id not provided, exiting"
+        exit 0
+fi
+if [ -z "$6" ]; then
+        echo "bundle type not provided, exiting"
+        exit 0
+fi
+
+echo "build id: $BUILD_ID"
+
+curl -XPOST -H 'Authorization: Bearer '$1'' -H "Content-type: application/json" -d '{
+  "'$6'":{"iv":["'$BUILD_ID'"]}
+}' 'https://prod.zebr-a.com/squidex/tools/draft/client-versions/'$5'/patch'
